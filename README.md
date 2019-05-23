80106c68df61ddea0e3de094ecf5b0660861d2d3
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-05-23 15:57:30 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # VTL-Test
 
 Test of a VTL connector for Pogues questionnaire descriptions
+
+
+## Usage with java-vtl-tools
+
+Checkout `https://github.com/statisticsnorway/java-vtl-tools.git`
+
+Add the connector as a dependency in the pom.xml
+
+```
+<dependency>
+    <groupId>fr.insee.vtl</groupId>
+    <artifactId>pogues-connector</artifactId>
+    <version>0.1</version>
+</dependency>
+```
+
+Edit the `no.ssb.vtl.tools.rest.Application`
+
+```
+@Bean
+    @Autowired
+    List<Connector> getConnectors() {
+        // ...
+        connectors.add(new PoguesConnector());
+    }
+```
--- src/main/java/fr/insee/vtl/connectors/PoguesConnector.java
+++ src/main/java/fr/insee/vtl/connectors/PoguesConnector.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 
@@ -27,25 +26,31 @@ public class PoguesConnector implements Connector {
 	@Override
 	public Dataset getDataset(String identifier) throws ConnectorException {
 
-		// Read the Simpsons questionnaire as a DOM
-		File fXmlFile = new File("src/main/resources/simpsons.xml");
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder;
+		// TODO: Franck
 		try {
-			dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(fXmlFile);
+			// Read the Simpsons questionnaire as a DOM
+			File fXmlFile = new File("src/main/resources/simpsons.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder;
+			try {
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(fXmlFile);
+			} catch (Exception e) {
+				throw new ConnectorException(e);
+			}
 		} catch (Exception e) {
-			throw new ConnectorException(e);
+			// Ignore.
 		}
-		
 		
 		StructureBuilder builder = StaticDataset.create();
 
 		builder.addComponent("ID", Role.IDENTIFIER, String.class); // Doit exister et être unique
 		builder.addComponent("NAME", Role.ATTRIBUTE, String.class); // Nom de la variable, role identifiant également
-		builder.addComponent("AGE", Role.ATTRIBUTE, Integer.class);
+		builder.addComponent("AGE", Role.ATTRIBUTE, Long.class);
 
-		return builder.build();
+		return builder
+				.addPoints("ID", "Hadrien", 32L)
+				.build();
 	}
 
 	@Override
